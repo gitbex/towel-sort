2a5d06543b7cb7763c140b93fb0afc8fddbcdb60
Bekhruz Mirrakhimov <bexruz@gmail.com>
2020-02-25 13:42:20 +0500

    init: test check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(arr){
+  var newarr = [];
+  if(arr === undefined || arr.length === 0){
+    return [];
+  } else {
+     for(var i = 0; i<arr.length; i++){
+        if(i % 2 === 0 || i === 0) {
+         for(var j= 0; j<arr[i].length; j++){
+            newarr.push(arr[i][j]);
+         }
+       } else {
+          for(var k= arr[i].length -1; k >= 0; k--){
+             newarr.push(arr[i][k]);
+          }
+       }
+  }
+  }
+ return newarr
 }
